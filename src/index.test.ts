2e67773a56a2762e92a33f015092278c956e8cc4
Francois Vongue <francoisvongue@gmail.com>
2022-03-13 11:02:09 +0200

    add async support

---
+++ src/index.test.ts
@@ -0,0 +1,159 @@
+import {AsyncObjectMapSpec, Map, ObjectMapSpec} from "./index";
+import {num, obj, Return} from "fp-way-core";
+
+describe('Map', () => {
+    type UserInfo = {
+        userId: string,
+        colorOfEyes: string,
+        role: 'user' | 'admin',
+        city: string,
+        location: [number, number],
+    }
+    type User = {
+        id: string,
+        firstName: string,
+        lastName: string,
+        email: string,
+        age: number,
+        userInfo: UserInfo
+    }
+    type UserInfoView = Omit<UserInfo, 'userId' | 'colorOfEyes'> & {
+        colorOfEyes: [number, number, number]
+    }
+    type UserView = {
+        name: string,
+        email: string,
+        userInfo: UserInfoView
+    }
+    type UserInfoWithUserView = UserInfoView & Omit<UserView, 'userInfo'>
+
+    const GetTestUserInfo = Return<UserInfo>(obj.DeepCopy({
+        userId: 'dsf79f8s98f7',
+        role: 'user',
+        colorOfEyes: '2FA8FF',
+        city: 'London',
+        location: [2141241, 509520523],
+    }));
+    const GetTestUser = Return<User>(obj.DeepCopy({
+        id: 'dsf79f8s98f7',
+        firstName: 'Rafael',
+        lastName: 'Nadal',
+        email: 'rn@mail.com',
+        age: 21,
+        userInfo: GetTestUserInfo()
+    }));
+    const GetTestUserInfoAsync = () => new Promise((res, rej) => {
+        setTimeout(() => {
+            res(GetTestUserInfo())
+        }, 350)
+    });
+    const MapColor = (s: string) => {
+        const groupEvery = (n: number, arr: any[]) => {
+            const result: any[] = [];
+            arr.forEach((v, i) => {
+                const resIndex = Math.floor(i / n);
+                if(Array.isArray(result[resIndex])) {
+                    (result[resIndex] as any[]).push(v);
+                } else {
+                    (result[resIndex] as any[]) = [v];
+                }
+            });
+            return result;
+        }
+        const rgb = groupEvery(2, [...s])
+            .map(([a, b]) => a + b)
+            .map(hex => Number.parseInt(hex, 16)) as [number, number, number];
+
+        return rgb;
+    }
+    const MapColorAsync = (s: string): Promise<[number, number, number]> => {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                res(MapColor(s));
+            }, 20);
+        })
+    }
+
+    it('get test async should return a user asynchronously', async (done) => {
+        const promise: UserInfo = GetTestUserInfoAsync() as any;
+        expect(promise?.role).toBeUndefined();
+
+        const result: UserInfo = await promise;
+        expect(result.role).toBeDefined();
+        done();
+    });
+
+    describe('sync spec', () => {
+        const UserInfoUserInfoViewMapSpec: ObjectMapSpec<UserInfo, UserInfoView> = {
+            map: [
+                ['colorOfEyes', MapColor, 'colorOfEyes']
+            ],
+            transfer: ['role', 'city', 'location']
+        }
+        const UserUserViewMapSpec: ObjectMapSpec<User, UserView> = {
+            map: [
+                ['userInfo', UserInfoUserInfoViewMapSpec ,'userInfo'],
+                [
+                    '',
+                    (v, o) => o.firstName + ' ' + o.lastName,
+                    'name'
+                ],
+            ],
+            transfer: ['email']
+        }
+
+        it('should map objects one to another', () => {
+            const user = GetTestUser();
+            const userView = Map(UserUserViewMapSpec, user);
+
+            expect(userView.name).toBeDefined();
+            expect(userView.email).toBeDefined();
+            expect(userView.userInfo).toBeDefined();
+            expect(userView.userInfo.city).toBeDefined();
+            expect(userView.userInfo.colorOfEyes).toBeInstanceOf(Array);
+            expect(userView.userInfo.location).toBeDefined();
+            expect(userView.userInfo.role).toBeDefined();
+            expect((userView as any as User)?.id).toBeUndefined(); // should be removed
+        })
+    })
+
+    describe('async spec', () => {
+        const AsyncUserInfoUserInfoViewMapSpec: AsyncObjectMapSpec<UserInfo, UserInfoView> = {
+            map: [
+                ['colorOfEyes', MapColorAsync, 'colorOfEyes'] // async mapping
+            ],
+            transfer: ['role', 'city', 'location'],
+            async: true
+        }
+        const UserUserViewMapSpec: AsyncObjectMapSpec<User, UserView> = {
+            map: [
+                // nested async mapping
+                ['userInfo', AsyncUserInfoUserInfoViewMapSpec ,'userInfo'],
+                // async mapping
+                [
+                    '',
+                    (v, o) => Promise.resolve(o.firstName + ' ' + o.lastName),
+                    'name'
+                ],
+            ],
+            transfer: ['email'],
+            async: true
+        }
+
+        it('should map objects one to another', async (done) => {
+            const user = GetTestUser();
+            const userView = await Map(UserUserViewMapSpec, user);
+
+            expect(userView.name).toBeDefined();
+            expect(userView.email).toBeDefined();
+            expect(userView.userInfo).toBeDefined();
+            expect(userView.userInfo.city).toBeDefined();
+            expect(userView.userInfo.colorOfEyes).toBeInstanceOf(Array);
+            expect(userView.userInfo.location).toBeDefined();
+            expect(userView.userInfo.role).toBeDefined();
+            expect((userView as any as User)?.id).toBeUndefined(); // should be removed
+
+            done();
+        })
+    })
+});
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,77 @@
-import {
-    Curry,
-    DataObject,
-    Exists,
-    FALSE,
-    Identity,
-    InCase,
-    IsOfType,
-    Not,
-    TRUE,
-    TypeOf,
-    Unary,
-    obj
-} from "fp-way-core";
+import {Curry, DataObject, Identity, InCase, IsOfType, Not, obj, Return, TypeOf, Unary} from "fp-way-core";
+import FromEntries = obj.FromEntries;
+
+type FPK = [Function, any[], string];
+function BuildObject(fnsWithParamsAndRKey: FPK[]): DataObject {
+    const resEntries = fnsWithParamsAndRKey
+        .map(([f, ps, rk]) => [rk,  f(...ps)] as [string,  any]);
+    return FromEntries(resEntries);
+}
+async function BuildObjectAsync(fnsWithParamsAndRKey: FPK[]): Promise<DataObject> {
+    const resEntries = await Promise.all(
+        fnsWithParamsAndRKey
+            .map(async ([f, ps, k]) => [k,  await f(...ps)] as [string,  any])
+    );
+    return FromEntries<DataObject>(resEntries);
+}
 
 export type ObjectMapper<O1> = (value: any | null, obj: O1) => any;
+export type AsyncObjectMapper<O1> = (value: any | null, obj: O1) => Promise<any>;
+export type AsyncObjectMapSpec<O1 extends DataObject, O2 extends DataObject> = {
+    map: [
+        keyof O1 | '',
+        | AsyncObjectMapper<O1>
+        | ObjectMapper<O1>
+        | ObjectMapSpec<any, any>
+        | AsyncObjectMapSpec<any, any>,
+        keyof O2
+    ][];
+    transfer?: Extract<keyof O1, keyof O2>[],
+    async: true
+}
 export type ObjectMapSpec<O1 extends DataObject, O2 extends DataObject> = {
     map: [keyof O1 | '', ObjectMapper<O1> | ObjectMapSpec<any, any>, keyof O2][];
     transfer?: Extract<keyof O1, keyof O2>[],
-    allowNull?: boolean
+    async?: false
 }
 export const Map: {
     <O1 extends DataObject, O2 extends DataObject>(
         mapSpec: ObjectMapSpec<O1, O2>,
-        src: O1
+        src: null | O1
     ): O2
 
     <O1 extends DataObject, O2 extends DataObject>(
-        mapSpec: ObjectMapSpec<O1, O2>,
-    ): Unary<O1, O2>
-} = Curry((mapSpec: ObjectMapSpec<any, any>, originalSrc) => {
-    const src = obj.DeepCopy(originalSrc);
-    const result = {};
-    const ObjOrNull = o => typeof o === 'object';
+        mapSpec: AsyncObjectMapSpec<O1, O2>,
+        src: null | O1
+    ): Promise<O2>
 
-    InCase([
-        [Not(ObjOrNull), () => {
-            throw Error(`Invalid input to obj.Map: src must be an object or null`)
-        }],
-        [o => o === null && !mapSpec?.allowNull, () => {
-            throw Error(`Invalid input to obj.Map: src can not be null`)
-        }]
-    ], src);
+    <O1 extends DataObject, O2 extends DataObject>(
+        mapSpec: ObjectMapSpec<O1, O2>,
+    ): Unary<null | O1, O2>
+
+    <O1 extends DataObject, O2 extends DataObject>(
+        mapSpec: AsyncObjectMapSpec<O1, O2>,
+    ): Unary<null | O1, Promise<O2>>
+} = Curry((
+    mapSpec: AsyncObjectMapSpec<any, any> | ObjectMapSpec<any, any>,
+    originalSrc: DataObject
+): any => {
+    if(!mapSpec)
+        throw Error(`Invalid input to Map: spec can not be null`);
+    if(!originalSrc)
+        throw Error(`Invalid input to Map: src can not be null`);
+
+    const src = obj.DeepCopy(originalSrc);
+    const ObjOrNull = o => typeof o === 'object';
+    const fnsWithParamsAndKey: FPK[] = [];
 
     if(mapSpec.transfer) {
         for(const prop of mapSpec.transfer) {
-            result[prop] = src?.[prop];
+            fnsWithParamsAndKey.push([
+                Return(src?.[prop as string]),
+                [null],
+                prop as string
+            ]);
         }
     }
 
@@ -52,201 +79,25 @@ export const Map: {
         const [sourceProp, mapperOrSpec, destinationProp] = row;
 
         if(IsOfType('function', mapperOrSpec)) {
-            result[destinationProp] = (mapperOrSpec as ObjectMapper<any>)(src?.[sourceProp] ?? null, src);
+            const params = [src?.[sourceProp as string] ?? null, src];
+            fnsWithParamsAndKey.push([
+                mapperOrSpec as ObjectMapper<any>,
+                params,
+                destinationProp as string
+            ]);
         } else if (IsOfType('object'), mapperOrSpec) {
-            result[destinationProp] = Map((mapperOrSpec as ObjectMapSpec<DataObject, any>), src?.[sourceProp] ?? null);
+            const params = [mapperOrSpec, src?.[sourceProp as string] ?? null];
+            fnsWithParamsAndKey.push([
+                Map,
+                params,
+                destinationProp as string
+            ]);
         } else {
             throw Error(`Invalid input to obj.Map: mapper can be either function or object, not ${TypeOf(mapperOrSpec)}`)
         }
     }
 
-    return result;
+    return (mapSpec.async ?? false)
+        ? BuildObjectAsync(fnsWithParamsAndKey)
+        : BuildObject(fnsWithParamsAndKey);
 });
-
-// == VALIDATOR
-export type ValidationSummary<T1 extends DataObject> = {
-    valid: boolean,
-    errorCount: number,
-    missingProperties: string[],
-    redundantProperties: string[],
-    errors: Record<keyof T1 | '_self', string[]>
-}
-namespace _ValidationSummary {
-    export const incErrCount = (s: ValidationSummary<any>) => {
-        s.errorCount++
-        s.valid = false
-    }
-    export const addErr = (k: string, msg: string, summary: ValidationSummary<any>) => {
-        if(IsOfType('array', summary.errors[k])) {
-            (summary.errors[k]).push(msg);
-        } else {
-            summary.errors[k] = [msg];
-        }
-        incErrCount(summary);
-    }
-    export const New = <T1>(): ValidationSummary<T1> => {
-        return {
-            valid: true,
-            errorCount: 0,
-            missingProperties: [],
-            redundantProperties: [],
-            errors: {} as Record<keyof T1 | '_self', string[]>
-        }
-    }
-    export const mergeNestedSummary = (
-        summary: ValidationSummary<any>,
-        key: string,
-        nestedSummary: ValidationSummary<any>,
-    ) => {
-        const prependKey = (v: string) => `${key}.${v}`
-
-        summary.valid = nestedSummary.valid && summary.valid;
-        summary.errorCount += nestedSummary.errorCount;
-        summary.missingProperties.push(...nestedSummary.missingProperties.map(prependKey));
-        summary.redundantProperties.push(...nestedSummary.redundantProperties.map(prependKey));
-
-        const nestedErrors: [any, any][] = obj.Entries(nestedSummary.errors)
-            .map(([nestedKey, v]) => [prependKey(nestedKey), v])
-
-        summary.errors = obj.FromEntries([
-            ...obj.Entries(summary.errors),
-            ...nestedErrors
-        ])
-    }
-}
-export type ValidationException = {
-    key: string,
-    value: any,
-    ruleIndex: number,
-    error: Error
-}
-export type ValidationOptions<T extends DataObject> = {
-    stopWhen?: (summary: ValidationSummary<T>) => boolean,
-    errorHandler?: (e: ValidationException) => string,
-    redundantIsError?: boolean,
-    optionalProps?: (keyof T)[],
-    isOptional?: boolean
-}
-export type PopulatedValidationOptions<T1 extends DataObject> = Required<ValidationOptions<T1>>;
-export const _defaultValidationOptions: PopulatedValidationOptions<any> = {
-    optionalProps: [],
-    redundantIsError: true,
-    stopWhen: FALSE,
-    errorHandler: ({key}) => `Could not validate property: ${key}`,
-    isOptional: false
-}
-export type ValidationPropertyRule<T1> = [
-    (v: any, o: T1) => boolean,
-        string | ((v: any, k: keyof T1) => string)
-];
-export const ValidationOptionsSym: unique symbol = Symbol.for('fp-way-validation-options');
-export type AnyValidationSpec =
-    & Record<string, any>
-    & { [ValidationOptionsSym]?: ValidationOptions<any> };
-export type ValidationSpec<T1 extends DataObject> =
-    & Record<keyof T1, ValidationPropertyRule<T1>[] | AnyValidationSpec>
-    & { [ValidationOptionsSym]?: ValidationOptions<T1> };
-export type ValidationSpecWithPopulatedOptions<T1 extends DataObject> =
-    & ValidationSpec<T1>
-    & { [ValidationOptionsSym]: PopulatedValidationOptions<T1> };
-export type _CheckPropsResult = {
-    missing: string[],
-    redundant: string[],
-    propsToCheck: string[],
-}
-export const _validationPreCheckProps = <T1 extends DataObject>(
-    spec: ValidationSpecWithPopulatedOptions<T1>,
-    o: T1
-): _CheckPropsResult => {
-    const declaredPropsToCheck = obj.Keys(spec);
-    const optionalProps = spec[ValidationOptionsSym].optionalProps;
-    const requiredProps = declaredPropsToCheck.filter(d => !optionalProps.includes(d));
-
-    const presentProps = obj.Entries(o)
-        .filter(([_, v]) => Exists(v))
-        .map(([k, _]) => k);
-
-    const missingRequiredProps = requiredProps.filter(r => !presentProps.includes(r));
-    const redundantProps = presentProps.filter(p => !declaredPropsToCheck.includes(p));
-    const propsToCheck = presentProps.filter(p => declaredPropsToCheck.includes(p));
-
-    return {
-        missing: missingRequiredProps,
-        propsToCheck: propsToCheck,
-        redundant: redundantProps,
-    }
-}
-
-export const Validate = <T extends DataObject>(
-    spec: ValidationSpec<T>,
-    obj: T
-): ValidationSummary<T> => {
-    const options: PopulatedValidationOptions<T> = obj.WithDefault(
-        _defaultValidationOptions,
-        spec[ValidationOptionsSym] ?? {}
-    );
-    const populatedSpec = (spec[ValidationOptionsSym] = options, spec) as ValidationSpecWithPopulatedOptions<T>
-
-    const summary: ValidationSummary<T> = _ValidationSummary.New();
-
-    if(!IsOfType("object", obj)) {
-        if(!options.isOptional || Exists(obj)) {
-            _ValidationSummary.addErr('_self', 'Value should be an object', summary)
-        }
-        return summary;
-    }
-
-    const {
-        propsToCheck,
-        missing,
-        redundant
-    } = _validationPreCheckProps(populatedSpec, obj);
-
-    if(missing.length) {
-        _ValidationSummary.incErrCount(summary);
-        summary.missingProperties = missing;
-    }
-    if(redundant.length) {
-        summary.redundantProperties = redundant;
-        if(options.redundantIsError) {
-            _ValidationSummary.incErrCount(summary);
-        }
-    }
-
-    for(let i = 0; i < propsToCheck.length; i++) {
-        if(options?.stopWhen(summary)) { return summary; }
-
-        const ptc = propsToCheck[i];
-        const keySpec: ValidationPropertyRule<T>[] | ValidationSpec<any> = spec[ptc];
-        const value = obj[ptc];
-
-        if(IsOfType('array', keySpec)) {
-            for(const rule of keySpec as ValidationPropertyRule<T>[]) {
-                const [validator, msgOrFn] = rule;
-
-                let rulePass;
-                try {
-                    rulePass = validator(value, obj);
-                } catch(e){
-                    const message = options.errorHandler({key: ptc, value, ruleIndex: i, error: e})
-                    _ValidationSummary.addErr(ptc, message, summary)
-                    continue;
-                }
-
-                if(!rulePass) {
-                    const message: string = InCase([
-                        [IsOfType('string'), Identity],
-                        [TRUE, f => f(value, ptc)],
-                    ], msgOrFn)
-
-                    _ValidationSummary.addErr(ptc, message, summary);
-                }
-            }
-        } else {
-            const nestedSummary = Validate(keySpec as ValidationSpec<T>, value)
-            _ValidationSummary.mergeNestedSummary(summary, ptc, nestedSummary)
-        }
-    }
-
-    return summary;
-}
